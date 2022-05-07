f92393fcc069ad4e2ab0ce30af7f57fd77395aad
Neryson <nerysongamer@gmail.com>
2022-05-07 18:06:12 -0300

    projeto netflix finalizado

---
+++ js/owl/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+
+    autoplay:true,
+autoplayTimeout:5000,
+autoplayHoverPause:false,
+
+
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
